912aa5ab0fb5cbba0834bc254abe89db0661a361
En-Chi Liu <enchi.liu@gmail.com>
2021-04-01 23:09:25 +0200

    added key to children in arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 // Project files
 import OrderConcise from "./components/OrderConcise";
+import OrderDetailed from "./components/OrderDetailed";
 import Information from "./data/orders.json";
-import "./styles/pages/style.css";
+import "./styles/style.css";
 
 export default function App() {
-  // Components
+  //Components
   const orderConciseArray = Information.map((item) => (
-    <OrderConcise information={item} />
+    <OrderConcise key={item.id} information={item} />
+  ));
+
+  const orderDetailedArray = Information.map((item) => (
+    <OrderDetailed key={item.id} information={item} />
   ));
 
   return (
     <div className="App">
-      <p>Testing</p>
+      <h1>Testing</h1>
+      <h2>Detailed</h2>
       <div className="list">
-      {orderConciseArray}
+      {orderDetailedArray}  c
+      <h2>Concise</h2>
+      {orderConciseArray}  
+      
       </div>      
     </div>
   );
