cefdc83885240ebc28d9dfb646f7c4c7193aca29
En-Chi Liu <enchi.liu@gmail.com>
2021-04-02 00:07:54 +0200

    added from order text to detailed order

--- src/components/OrderConcise.jsx
+++ src/components/OrderConcise.jsx
@@ -1,4 +1,8 @@
+// NPM Package
+import { Link } from "react-router-dom";
+
 // Project files
+
 import StatusIcon from "./StatusIcon";
 import StatusWording from "./StatusWording";
 
@@ -8,10 +12,13 @@ export default function OrderConcise({ information }) {
       <div className="left">
         <StatusIcon orderStatus={information.status} />
       </div>
-      <div className="right">
-        <p className="sender">{information.sender}</p>
-        <StatusWording orderStatus={information.status} />
-      </div>
+      {/* =========TODO Check this link===========*/}
+      <Link to={`result/${information.id}`}>
+        <div className="right">
+          <p className="sender">{information.sender}</p>
+          <StatusWording orderStatus={information.status} />
+        </div>
+      </Link>
     </div>
   );
 }
