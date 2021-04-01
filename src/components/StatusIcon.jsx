329911244a18380f40a000e90a8bcdea9fbdafef
En-Chi Liu <enchi.liu@gmail.com>
2021-04-01 18:25:54 +0200

    Imported FontAwesome

--- src/components/StatusIcon.jsx
+++ src/components/StatusIcon.jsx
@@ -1,10 +1,14 @@
+// NPM pacakge
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faTruck, faParachuteBox, faSatelliteDish} from '@fortawesome/free-solid-svg-icons'
+
 export default function StatusIcon(orderStatus) {
   return (
     <div className="StatusIcon">
-      {orderStatus === "order-info-received" && true}
-      {orderStatus === "on-the-way" && true}
-      {orderStatus === "delivered" && true}
-      {orderStatus === "ready-for-pickup" && true}
+      {orderStatus === "order-info-received" && <FontAwesomeIcon icon={faSatelliteDish} />}
+      {orderStatus === "on-the-way" && <FontAwesomeIcon icon={faTruck} />}
+      {orderStatus === "delivered" && <FontAwesomeIcon icon={faCheckCircle} />}
+      {orderStatus === "ready-for-pickup" && <FontAwesomeIcon icon={faParachuteBox} />}
     </div>
   );
 }
