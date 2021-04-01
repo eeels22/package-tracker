639620e50a54290b170009ce33d8d070082c5a41
En-Chi Liu <enchi.liu@gmail.com>
2021-04-02 00:08:24 +0200

    Added BrowerRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
+// NPM packages
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 // Project files
-import OrderConcise from "./components/OrderConcise";
-import OrderDetailed from "./components/OrderDetailed";
-import Information from "./data/orders.json";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Parcels from "./pages/Parcels";
+import Results from "./pages/Results";
 import "./styles/style.css";
 
 export default function App() {
-  //Components
-  const orderConciseArray = Information.map((item) => (
-    <OrderConcise key={item.id} information={item} />
-  ));
 
-  const orderDetailedArray = Information.map((item) => (
-    <OrderDetailed key={item.id} information={item} />
-  ));
 
   return (
     <div className="App">
-      <h1>Testing</h1>
+      <Header/>
+      <BrowserRouter>
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={Parcels} path="/parcels/:id" />
+          <Route component={Results} path="/results/:id" />
+        </Switch>        
+       </BrowserRouter>
+    </div>
+  );
+}
+
+/* <h1>Testing</h1>
       <h2>Detailed</h2>
       <div className="list">
       {orderDetailedArray}  c
       <h2>Concise</h2>
-      {orderConciseArray}  
-      
-      </div>      
-    </div>
-  );
-}
+      {orderConciseArray}   */
