c5946681dd189e35f2967a9eff5ee2493f5e6a80
En-Chi Liu <enchi.liu@gmail.com>
2021-04-01 22:24:21 +0200

    started adding <p> tags

---
+++ src/components/OrderDetailed.jsx
@@ -0,0 +1,31 @@
+import StatusIcon from "./StatusIcon";
+import StatusWording from "./StatusWording";
+
+export default function OrderConcise({ information }) {
+  return (
+    <div className="OrderConcise">
+      <StatusIcon orderStatus={information.status} />
+      <p className="data-label">Sender</p>
+      <p className="data-actual">{information.sender}</p>
+      <p className="data-label">Status</p>
+      <StatusWording orderStatus={information.status} />
+      {/* TODO convert to long date */}
+      <p className="data-label">Estimated delivery date</p>
+      <p className="data-actual">{information.eta}</p>
+      <p className="data-label">Parcel ID</p>
+      <p className="data-actual">{information.parcel_id}</p>
+      <p className="data-label">Pickup location</p>
+      <p className="data-actual">{information.location_name}</p>
+      <p className="data-label">Phone number</p>
+      <p className="data-actual">{information.user_phone}</p>
+      <p className="data-label">Verification required?</p>
+      <p className="data-actual">{information.verification_required}</p>
+      <p className="data-label">Notes</p>
+      {/* TODO Check if it's null */}
+      <p className="data-actual">{information.notes}</p>
+      {/* TODO convert to long date and time */}
+      <p className="data-label">Last updated</p>
+      <p className="data-actual">{information.last_updated}</p>
+    </div>
+  );
+}
