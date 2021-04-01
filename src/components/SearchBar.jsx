1d4a720d1b88114de2c01202a3da9b20a3383acb
En-Chi Liu <enchi.liu@gmail.com>
2021-04-01 17:50:48 +0200

    UpdatedSearchBar.jsx

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+// NPM packages
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function SearchBar() {
+  // Local state
+  const [query, setQuery] = useState("");
+
+  // Property
+  const history = useHistory();
+
+  // Methods
+  function search(event) {
+    event.preventDefault();
+
+    history.push(`/results/${query}`);
+  }
+
+  <form onSubmit={search} className="SearchBar">
+    <input
+      type="text"
+      placeholder="Search by sender name"
+      value={query}
+      onChange={(event) => setQuery(event.target.value)}
+    />
+    <input type="submit" value="Search" />
+  </form>;
+}
