ddf2ca79012e221079a580c770d0d7ca949509a2
En-Chi Liu <enchi.liu@gmail.com>
2021-04-01 20:24:27 +0200

    Updated OrderConcise component

--- src/components/OrderConcise.jsx
+++ src/components/OrderConcise.jsx
@@ -1,12 +1,13 @@
-import Information from "../data/orders.json";
+import {status, sender} from "../data/orders.json";
 import { StatusIcon } from "./StatusIcon";
 
 export default function OrderConcise() {
+
   return (
     <div className="OrderConcise">
-      <StatusIcon orderStatus={Information.status} />
-      <p className="sender">{Information.sender}</p>
-      <p className="status">{Information.status}</p>
+      <StatusIcon orderStatus={status} />
+      <p className="sender">{sender}</p>
+      <p className="status">{status}</p>
     </div>
   );
 }
