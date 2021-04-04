eaeffa1557beee0aaa23e08076b061439642c69a
En-Chi Liu <enchi.liu@gmail.com>
2021-04-04 15:26:56 +0200

    Added 'back to home' link

--- src/pages/Parcel.jsx
+++ src/pages/Parcel.jsx
@@ -1,3 +1,6 @@
+// NPM package
+import { Link } from "react-router-dom";
+
 // Project files
 import Information from "../data/orders.json";
 import OrderDetailed from "../components/OrderDetailed";
@@ -5,14 +8,17 @@ import OrderDetailed from "../components/OrderDetailed";
 export default function Parcel({ match }) {
   // Constants
   const routerID = match.params.id;
-  const parcel = Information.find((item) => item.parcel_id === routerID); // which id field?
+  const parcel = Information.find((item) => item.id === routerID); // should it be parvel_id instead?
 
   return (
     <div id="parcel" className="container">
       <h1>Parcel details</h1>
-      <div>
+      <div className="OrderDetailed">
         <OrderDetailed key={parcel.id} information={parcel} />
       </div>
+      <Link className="back-to-home" to="/">
+        Back to home
+      </Link>
     </div>
   );
 }
