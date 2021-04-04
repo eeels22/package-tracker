ed3c2dd316a15f584963d52ccb195ec065b33041
En-Chi Liu <enchi.liu@gmail.com>
2021-04-04 15:11:22 +0200

    Added linked logo to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
-export default function Header() {
-    //Constants
-    const logoHeaderObject = require(`../assets/logo.svg`);
-    const logoHeaderURL = logoHeaderObject.default;
+// NPM package
+import { Link } from "react-router-dom";
 
-    return (
-        <header className="Header">
-            <p>Testing Header</p>   
-            <img src={logoHeaderURL} alt="YellowCorp logo" className="logo-header" />                     
-        </header>
-    )
+// Project files
+import SearchBar from "./SearchBar";
+
+export default function Header() {
+  //Constants
+  const logoHeaderObject = require(`../assets/logo.svg`);
+  const logoHeaderURL = logoHeaderObject.default;
+
+  return (
+    <header className="Header">
+      <Link to="/">
+        <img
+          src={logoHeaderURL}
+          alt="YellowCorp logo"
+          className="logo-header"
+        />
+      </Link>
+      <SearchBar/>
+    </header>
+  );
 }
