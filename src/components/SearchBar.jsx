5e01786c48d0984e64e9e789188ba1678d50e67f
En-Chi Liu <enchi.liu@gmail.com>
2021-04-05 13:25:06 +0200

    added classnames

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,19 +17,22 @@ export default function SearchBar() {
   }
 
   return (
-    <div className="SearchBar">
-      <form onSubmit={search}>
-        <label htmlFor="sender-search" className="search-label">
+    <div>
+      <form onSubmit={search} className="SearchBar">
+        <label htmlFor="sender-search" className="search-label" id="search-label">
           Search parcels by sender
         </label>
-        <input
-          type="text"
-          id="sender-search"
-          placeholder="Enter name of sender"
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-        />
-        <input type="submit" value="Search" />
+        <div className="search-input">
+          <input
+            type="text"
+            id="sender-search"
+            placeholder="Enter name of sender"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            className="search-box"
+          />
+          <input type="submit" value="Search" className="search-button" />
+        </div>
       </form>
     </div>
   );
