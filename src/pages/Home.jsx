0ab2ff4aebad286453b868b003078b460b5a6282
En-Chi Liu <enchi.liu@gmail.com>
2021-04-05 16:36:34 +0200

    moved image up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 // NPM package
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   // Constants
@@ -8,17 +8,17 @@ export default function Home() {
 
   return (
     <div id="home" className="container">
-      <h1>Track your parcels with ease</h1>
-      <h2>1. View all parcels</h2>
-      <Link to="/list">View all parcels</Link>
-      <h2>2. Search for a parcel by sender</h2>
-      <p>Use the search bar above to find parcels from a particular sender.</p>
-
       <img
         src={imageURL}
         alt="Illustration of a delivery van"
         className="image-van"
       />
+      <h1>Track your parcels with ease</h1>
+      <h2>1. View all parcels</h2>
+      <p>Get an overview of all traced parcels.</p>
+      <Link to="/list">View all parcels</Link>
+      <h2>2. Search for a parcel by sender</h2>
+      <p>Use the search bar above to find parcels from a particular sender.</p>
     </div>
   );
 }
